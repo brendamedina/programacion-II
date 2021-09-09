eafc2927d97b674cfde559e911f05311f64c8a40
brendamedina <79316555+brendamedina@users.noreply.github.com>
2021-09-09 13:55:48 -0500

    Update sum_rest_mult_div-funcion.py

--- tareas/ejercicios_b/sum_rest_mult_div-funcion.py
+++ tareas/ejercicios_b/sum_rest_mult_div-funcion.py
@@ -1,17 +1,3 @@
-'''num1 = 40
-num2 = 40
-
-suma = num1 + num2
-print ("suma=", suma)
-
-resta = num1 - num2
-print ("resta=", resta)
-
-mult = num1 * num2
-print ("multiplicación=", mult)
-
-div = num1 + num2
-print ("división=", div)'''
 
 print ("Ingrese un número")
 num1 = float(input())
@@ -23,8 +9,20 @@ num2 = float(input())
 
 def calcular ():
     suma = num1 + num2
+    resta = num1 - num2
+    mult = num1 * num2
+    div = num1 / num2
     if operador == "+":
-        print (suma)
+        return suma
+    if operador == "-":
+        return resta
+    if operador == "*":
+        return mult
+    if operador == "/":
+        return div
+    
+
+print ("resultado:" ,calcular())
 
 
     
