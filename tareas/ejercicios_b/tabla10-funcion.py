dfa3fe7a38235e787a35cd59de5c36facd51c4e0
brendamedina <79316555+brendamedina@users.noreply.github.com>
2021-09-09 14:18:20 -0500

    Add files via upload

---
+++ tareas/ejercicios_b/tabla10-funcion.py
@@ -0,0 +1,14 @@
+
+def multiplicar():
+    x = 0
+    y = 10
+     
+    while x<10:
+        x = x + 1
+        resultado = (y, "*", x, "=", y*x)
+        print (resultado)
+
+(multiplicar())
+
+
+
